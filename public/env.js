9aa6a80701ea3aab763bc2d1a9f9bd41f42f2c49
Katrina Steen <s.katrina@y7mail.com>
2020-08-18 02:12:39 +1000

    added user age calculation and validation

--- public/env.js
+++ public/env.js
@@ -8,18 +8,23 @@
 //index to keep track of profile in a stack
 let profileIndex = 0;
 let matchList = [];
+//tracks age of user (profile will not be created/updated if user under age)
+let userAge = "";
 //current user, initialised to no information and locally stored photograph
 let userProfile = {
     profName: '',
     age: null,
     bio: '',
     pic: 'assets/UserPic.jpg',
-    email: ''
+    email: '',
+    dob: ''
 }
 
+//tracks X coordinants for mouse on down and up to detect drag/swipe
 let downMouseX = null;
 let upMouseX = null;
 
+//collection of suitor profiles
 let profileStack = [
     {
         profName: 'Izabelle',
@@ -63,7 +68,7 @@ let profileStack = [
 
 ]
 
-
+//sends request to server to send the user an email 
 const sendMatchMail = (message) => {
     //the text to be sent
     //let message = "You have been matched with " + profileStack[profileIndex].profName;
@@ -91,31 +96,69 @@ const sendMatchMail = (message) => {
 }
 
 
-const createUserProfile = () => {
+const calculateAge = (testDob) => {
+    let now = new Date();
+    let dob = new Date(testDob);
+    //calculate age with assumption bithday for the year has occurred
+    let age = (now.getFullYear() - dob.getFullYear());
+    //if the birthmonth is yet to occur, take a year off age
+    if (dob.getMonth() > now.getMonth()) {
+        age--;
+    }
+    //if in the birthmonth and the day is yet to occur, take a year off age
+    if (dob.getMonth() == now.getMonth()) {
+        if (dob.getDate() > now.getDate()) {
+            age--;
+        }
+    }
+    return age;
+}
 
+const createUserProfile = () => {
     userProfile.profName = $('#firstName').val();
-    userProfile.age = $('#age').val();
     userProfile.bio = $('#bio').val();
     //    userProfile.pic = $('#pic').val(); //I will remove this from form and select one of my own.
     userProfile.email = $('#email').val();
-
+    userProfile.dob = $('#dob').val();
+    //for ease of use, store calculated age
+    userProfile.age = userAge;
     //update any web displays with the created information
     userProfileUpdate();
 }
 
-
-const getMatchingFunction = () => {
+const validateForm = () => {
+    let oldEnough = false;
+    //if form date input is valid, check age
+    if (dob.checkValidity()) {
+        let formAge = calculateAge($('#dob').val());
+        //if under 18, alert user
+        if (formAge > 17) {
+            oldEnough = true;
+        } else {
+            alert('You must be 18 years or older to use this service');
+        }
+    }
     //only proceed if inputs are valid
-    let formValid = (firstName.checkValidity() && dob.checkValidity() && email.checkValidity() && bio.checkValidity())
+    let formValid = (oldEnough && firstName.checkValidity() && email.checkValidity() && bio.checkValidity())
     if (formValid) {
+        return true;
+    }
+}
+//if form inputs are valid, presents first match
+const getMatchingFunction = () => {
+    //only if form is valid, close form and display matches
+    if (validateForm()) {
+        //put top profile in stack in the view
+        displayProfile(0);
         //set our user's profile
         createUserProfile();
         //put top profile in stack in the view
-        displayProfile(0);
-        //hide the profile entry container/pane
-        $('#profileEntry').addClass('hidden');
+        // displayProfile(0);
         //shows the presenting profile pane
         $('#matching').removeClass('hidden');
+        //hide the profile entry container/pane
+        $('#profileEntry').addClass('hidden');
+
     }
 }
 
@@ -153,7 +196,7 @@ const constructMatchesDisplay = () => {
     let listContents = '';
     //for each element (profile) in the matchlist, add a row displaying its picture and name to the string
     matchList.forEach((profile, index) => {
-        listContents += '<div class="row"><div class="matchRow"><img src="' + profile.pic + '" class="miniImg circle" alt=""></div><div class="matchRow">' + profile.profName + ', age</div></div>';
+        listContents += '<div class="row"><div class="matchRow"><img src="' + profile.pic + '" class="miniImg circle" alt=""></div><div class="matchRow"><h5>' + profile.profName + '</h5></div></div>';
 
     })
     //then fill the constructed list with created html
@@ -231,13 +274,7 @@ const newProfileFunction = () => {
     $('#profileEntry').removeClass('hidden');
 }
 
-//GET RID OF THISE AFTER ALL TESTING DONE!!!
-const submitFunction = () => {
-    alert('got ere');
-    let formValid = (firstName.checkValidity() && dob.checkValidity() && email.checkValidity() && bio.checkValidity())
-    alert(formValid);
-    //alert(bio.checkValidity());
-}
+
 $(document).ready(function () {
     //reinitialise form inputs
     M.updateTextFields();
@@ -280,9 +317,7 @@ $(document).ready(function () {
     //====================================
 
     $('#nopeButton').click(nopeSelectedFunction)
-    $('#profileButton').click(getMatchingFunction)
-    //testing validation and form submission
-    $('#profileButton1').click(submitFunction)
+    $('#getMatchingButton').click(getMatchingFunction)
 
     // movementX
     // $('#matching').movementX(checkDragFunction)
@@ -301,150 +336,3 @@ $(document).ready(function () {
     */
 
 })
-
-//=========================OTHER STUFF=====================
-
-
-const getJournals = () => {
-    $.get('/api/journal', (journals) => {
-        if (journals.length > 0) {
-            $('#journal').empty()
-            journals.forEach(journal => {
-                appendJournal(journal)
-            })
-        }
-    })
-}
-
-
-const commentPost = (element) => {
-    let test = $(element)
-    postId = test.attr('value')
-    //console.log(element.val())
-}
-const newJournal = () => {
-    let text = $('#journalText').val()
-    if (username == null) {
-        username = 'Guest'
-        console.log('guest')
-    }
-    console.log(username)
-    let data = {
-        text: text,
-        author: username
-    }
-    $.ajax({
-        url: '/api/journal',
-        contentType: 'application/json',
-        data: JSON.stringify(data), // access in body
-        type: 'POST',
-        success: function (result) {
-            console.log(result)
-        }
-    });
-}
-
-
-
-const submitPost = () => {
-    let text = $('#commentText').val()
-    if (username == null) {
-        username = 'Guest'
-        console.log('guest')
-    }
-    console.log(username)
-    let data = {
-        _id: postId,
-        text: text,
-        author: username
-    }
-    console.log(data)
-
-    $.ajax({
-        url: '/api/journal',
-        contentType: 'application/json',
-        data: JSON.stringify(data), // access in body
-        type: 'PUT',
-        success: function (result) {
-            console.log(result)
-        }
-    });
-
-}
-
-const login = () => {
-    let user = $('#username').val()
-    if (user.length <= 0) {
-        username = 'Guest'
-        console.log('guest')
-    } else {
-        username = user
-    }
-    $('#buttonLogin').hide()
-
-    $('#user').html(username)
-
-}
-
-
-//handle journal
-const appendJournal = (journal) => {
-    // this iterates through all the comments and creates an object containing all of the comments
-    let jComments = $("<div class='col container'><div>");
-    journal.comments.forEach((comment) => {
-        let thisComment = "<div class='col s12 comment'><i class='material-icons left' style='color:white font-size: larger;'>chat_bubble_outline</i>" + comment.text + " by <b>" + comment.author + "</b></div>"
-        jComments.append(thisComment)
-    })
-    let temp = jComments.html()
-    let jString = "<div class='col s12 journalBox'>\
-    <div class='col s8'>"+ journal.author + "</div><div class='col s4'>" + journal.date + "</div>\
-    <div class='col s12 journalText'>"+ journal.text + "</div>\
-    <div class='col s12 commentsContainer row'>"+ temp + "</div>\
-    <div class='col s12 center'><a id='buttonComment' \
-    onclick=commentPost(this) value="+ journal._id + " class='waves-effect waves-light btn modal-trigger' href='#modalComments'>Comment</a><div>\
-  </div>";
-
-
-    let jEntry = $(jString)
-    //jEntry.append('Sto cazzo')
-
-    $('#journal').append(jEntry)
-
-}
-
-/*
-$(document).ready(function () {
-
-    console.log('Ready')
-
-    setInterval(() => {
-        getJournals()
-    }, 5000)
-    //test get call
-    $.get('/test?user_name="Fantastic User"', (result) => {
-        console.log(result)
-    })
-    // initialise the tabs
-    $('.tabs').tabs();
-
-    //initialise the map
-    var map = L.map('worldMap').setView([-37.7, 145], 10);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([-37.5, 144.7]).addTo(map)
-        .bindPopup('Bogan Castle')
-    L.marker([-37.8, 145.1]).addTo(map)
-        .bindPopup('VB Fortress')
-
-
-
-
-
-    $('.modal').modal();
-    getJournals()
-
-})
-*/
